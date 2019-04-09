ba97019de64987d0dd00243ace83341eee51e272
Moritz Hartmeier <moritz@scandit.com>
2019-04-09 13:44:56 +0200

    Merge pull request #126 from Scandit/feature/SDK-10408-support-for-android-camera-api-2
    Feature/sdk 10408 support for android camera api 2
    
    (cherry picked from commit 48c004fd953b7637f5443002f233fa077b38e734)

--- samples/MatrixScanSample/android/app/src/main/java/com/scandit/matrixscansample/MainApplication.java
+++ samples/MatrixScanSample/android/app/src/main/java/com/scandit/matrixscansample/MainApplication.java
@@ -22,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ScanditPackage()
-      );
+      return Arrays.asList(new MainReactPackage(), new ScanditPackage());
     }
 
     @Override
