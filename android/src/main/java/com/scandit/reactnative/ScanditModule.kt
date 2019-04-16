1cf8b209077bb20a8b17ca9227f746c19ac411a0
Tomasz Jurkowski <tomasz@scandit.com>
2019-04-16 14:05:43 +0200

    Settings the Android Camera API can be now done from JS layer.

--- android/src/main/java/com/scandit/reactnative/ScanditModule.kt
+++ android/src/main/java/com/scandit/reactnative/ScanditModule.kt
@@ -5,12 +5,20 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.scandit.barcodepicker.ScanditLicense
 
-class ScanditModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext)  {
+class ScanditModule(
+    reactContext: ReactApplicationContext,
+    val cameraApiSetting: CameraApiSetting
+) : ReactContextBaseJavaModule(reactContext)  {
 
     override fun getName(): String = "ScanditModule"
 
     @ReactMethod
     fun setAppKey(key: String) {
         ScanditLicense.setAppKey(key)
+    }
+
+    @ReactMethod
+    fun setCameraApi(newCameraApi: Int) {
+        cameraApiSetting.cameraApi = newCameraApi
     }
 }
